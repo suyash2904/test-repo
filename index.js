33b858da92cd2295e98e5b77163fcb84162c5431
suyash2904 <suyashingawale2904@gmail.com>
2023-07-05 22:06:07 +0530

    checking branch

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-//adadadad
+//adadadad;
+//adada
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
